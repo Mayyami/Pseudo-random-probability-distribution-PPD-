011d915ab2fbaae3ad3939ecc239ecdbff65852c
ARTEM <popa220099@gmail.com>
2026-03-04 20:36:45 +0300

    Fix some bugs with retries

--- PPD.py
+++ PPD.py
@@ -4,7 +4,7 @@ import random
 
 def pseudo_random(max_try, retry):
     c0 = find_c(max_try)
-    print("Константа 'c' для данного шанса составляет:",c0)
+    print("Константа 'c' для данного шанса составляет:", c0)
     c = c0
     now_retry = 1
     success_try = 0
@@ -22,10 +22,12 @@ def pseudo_random(max_try, retry):
             print("no...")
             c += c0
             now_retry += 1
-            print(f"Попыток до гаранта: {(max_try-now_retry)+1}")
+            print(f"Попыток до гаранта: {(max_try - now_retry) + 1}")
             print("-------------------------------")
     win_rate = success_try / retry
-    return(success_try, win_rate)
+    return success_try, win_rate
+
+
 s = 1
 while s == 1:
     s = 1
@@ -36,7 +38,7 @@ while s == 1:
     if retry <= 0:
         print("Количество попыток должно быть больше нуля")
     else:
-        s = pseudo_random(n, 100)
+        s = pseudo_random(n, retry)
         print("Количество удачных попыток:", s[0])
         print("Средний шанс на победу:", s[1])
     s = int(input("Нажмите 1 чтобы начать заново или 0 чтобы закончить программу:"))
